afeeb4ca01f732b473b4103ba116f02654805ff7
Timur Mukhitdinov <TUMukhitdinov@sberbank.ru>
2021-11-30 21:58:17 +0300

    initial commit

---
+++ src/test/java/InitialCheck.java
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InitialCheck {
+
+    @Test
+    void checkItWorks() {
+        Assertions.assertThat(true)
+                .isTrue();
+    }
+}
